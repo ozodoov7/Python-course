4b0493670585c36d5ddd823c3a31638185489e9f
ozodoov7 <javohirozodov52@gmail.com>
2026-04-23 19:11:08 +0500

    homework of 3rd lesson

---
+++ list_and_tuple_hw.py
@@ -0,0 +1,42 @@
+# n1
+# a=int(input("Qaysi sondan? "))
+# b=int(input("Qaysi songacha? "))
+# even=[]
+# i=0
+# for a in range(a,b):
+#     if a % 2 == 0:
+#         even.append(a)
+# print(even)
+
+#n2
+# t=(1,2,3,4,5,6,7,8,9,10)
+# print(t[3], t[-4])
+
+# n3
+# lst=[(10, 20, 40), (40, 50, 60), (70, 80, 90)]
+# new_lst=[]
+# for i in lst:
+#     t=i[:-1]+(100,)
+#     new_lst.append(t)
+# print(new_lst)
+
+# n4
+# word=input(">>> ")
+# t=[]
+# for i in word:
+#     t.append(i)
+# t=tuple(t)
+# print(t)
+
+# n5
+# lst=[ [1,2,3], [4,5,6], [10,11,12], [7,8,9] ]
+# print(max(lst, key=list))
+
+# n6
+# lst=[1,2,3,4]
+# word=input("So'z kiriting: ")
+# for i in range(len(lst)):
+#     a=lst.pop(i)
+#     a=str(a)
+#     lst.insert(i, word+a)
+# print(lst)
