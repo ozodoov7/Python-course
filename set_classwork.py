55d9269baf99b731432c0f8eb63df6e4b9786968
ozodoov7 <javohirozodov52@gmail.com>
2026-04-24 19:43:37 +0500

    Set methods

--- set_classwork.py
+++ set_classwork.py
@@ -1,11 +1,39 @@
-set1={1,2,3,4,5,8,54,32,54,98,65}
+# set1={1,2,3,4,5,8,54,32,54,98,65}
 # print(set1)
 
 # for i in set1:
     # print(i, end=" ")
 
-set2 = {"Olma","Anor", 'Gilos', "O'rik"}
-if "Anor" in set2:
-    print("Yes")
-else:
-    print("No")
+# set2 = {"Olma","Anor", 'Gilos', "O'rik"}
+# if "Anor" in set2:
+#     print("Yes")
+# else:
+#     print("No")
+# set1 = {'olma', 'anor', 'gilos', 'banan', 'uzum'}
+# set2 = {'qovun', 'tarvuz', 'uzum', 'banan', 'olma'}
+
+# set3 = set1.intersection(set2)
+# print(set3)
+
+# set1.intersection_update(set2)
+# print(set1)
+
+# set3 = set1.difference(set2)
+# set3 = set2.difference(set1)
+# print(set3)
+
+# set1.difference_update(set2)
+# print(set1)
+
+# set3 = set1.symmetric_difference(set2)
+# print(set3)
+
+# set1.symmetric_difference_update(set2)
+# print(set1)
+
+
+# set1 = {1,2,3,4,5,6,7,8,9,10}
+# set2 = {4,5,6}
+
+# print(set2.issubset(set1))
+# print(set1.issuperset(set2))
