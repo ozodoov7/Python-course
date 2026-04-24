ecf4b1704b3b3faa89df694c326c05f6866cc433
ozodoov7 <javohirozodov52@gmail.com>
2026-04-24 18:45:48 +0500

    Set

--- set_classwork.py
+++ set_classwork.py
@@ -1,2 +1,11 @@
 set1={1,2,3,4,5,8,54,32,54,98,65}
-print(set1)
+# print(set1)
+
+# for i in set1:
+    # print(i, end=" ")
+
+set2 = {"Olma","Anor", 'Gilos', "O'rik"}
+if "Anor" in set2:
+    print("Yes")
+else:
+    print("No")
