78456092cd6495c8b55ec35306a8ffb99b2f68eb
ozodoov7 <javohirozodov52@gmail.com>
2026-04-28 18:54:00 +0500

    Dictionary homework

--- dict_homework.py
+++ dict_homework.py
@@ -62,70 +62,70 @@
 #         print(f"Eng kam sotilgan: {i}")
 
 # n6
-books = {
-    "O'tkan kunlar": {
-        "muallifi": "Abdulla Qodiriy",
-        "janri": "Roman",
-        "chop etilgan yili": 1926,
-        "tarjimalar soni": 5
-    },
-    "Mehrobdan chayon": {
-        "muallifi": "Abdulla Qodiriy",
-        "janri": "Roman",
-        "chop etilgan yili": 1929,
-        "tarjimalar soni": 3
-    },
-    "Kecha va kunduz": {
-        "muallifi": "Cho'lpon",
-        "janri": "Roman",
-        "chop etilgan yili": 1934,
-        "tarjimalar soni": 4
-    },
-    "Sarob": {
-        "muallifi": "Abdulla Qahhor",
-        "janri": "Roman",
-        "chop etilgan yili": 1935,
-        "tarjimalar soni": 2
-    },
-    "Ulug'bek xazinasi": {
-        "muallifi": "Odil Yoqubov",
-        "janri": "Tarixiy roman",
-        "chop etilgan yili": 1974,
-        "tarjimalar soni": 6
-    },
-    "Don Kixot": {
-        "muallifi": "Migel de Servantes",
-        "janri": "Roman",
-        "chop etilgan yili": 1605,
-        "tarjimalar soni": 50
-    },
-    "Urush va tinchlik": {
-        "muallifi": "Lev Tolstoy",
-        "janri": "Tarixiy roman",
-        "chop etilgan yili": 1869,
-        "tarjimalar soni": 45
-    },
-    "Alkimyogar": {
-        "muallifi": "Paulo Koelyo",
-        "janri": "Falsafiy roman",
-        "chop etilgan yili": 1988,
-        "tarjimalar soni": 80
-    },
-    "1984": {
-        "muallifi": "Jorj Oruell",
-        "janri": "Antiutopik roman",
-        "chop etilgan yili": 1949,
-        "tarjimalar soni": 65
-    },
-    "Kichkina shahzoda": {
-        "muallifi": "Antuan de Sent-Ekzyuperi",
-        "janri": "Falsafiy ertak",
-        "chop etilgan yili": 1943,
-        "tarjimalar soni": 300
-    }
-}
-book=input("Kitob nomini kiriting: ")
-book=book.lower()
-for i in books:
-    if i.lower() == book:
-        print(books[i])
+# books = {
+#     "O'tkan kunlar": {
+#         "muallifi": "Abdulla Qodiriy",
+#         "janri": "Roman",
+#         "chop etilgan yili": 1926,
+#         "tarjimalar soni": 5
+#     },
+#     "Mehrobdan chayon": {
+#         "muallifi": "Abdulla Qodiriy",
+#         "janri": "Roman",
+#         "chop etilgan yili": 1929,
+#         "tarjimalar soni": 3
+#     },
+#     "Kecha va kunduz": {
+#         "muallifi": "Cho'lpon",
+#         "janri": "Roman",
+#         "chop etilgan yili": 1934,
+#         "tarjimalar soni": 4
+#     },
+#     "Sarob": {
+#         "muallifi": "Abdulla Qahhor",
+#         "janri": "Roman",
+#         "chop etilgan yili": 1935,
+#         "tarjimalar soni": 2
+#     },
+#     "Ulug'bek xazinasi": {
+#         "muallifi": "Odil Yoqubov",
+#         "janri": "Tarixiy roman",
+#         "chop etilgan yili": 1974,
+#         "tarjimalar soni": 6
+#     },
+#     "Don Kixot": {
+#         "muallifi": "Migel de Servantes",
+#         "janri": "Roman",
+#         "chop etilgan yili": 1605,
+#         "tarjimalar soni": 50
+#     },
+#     "Urush va tinchlik": {
+#         "muallifi": "Lev Tolstoy",
+#         "janri": "Tarixiy roman",
+#         "chop etilgan yili": 1869,
+#         "tarjimalar soni": 45
+#     },
+#     "Alkimyogar": {
+#         "muallifi": "Paulo Koelyo",
+#         "janri": "Falsafiy roman",
+#         "chop etilgan yili": 1988,
+#         "tarjimalar soni": 80
+#     },
+#     "1984": {
+#         "muallifi": "Jorj Oruell",
+#         "janri": "Antiutopik roman",
+#         "chop etilgan yili": 1949,
+#         "tarjimalar soni": 65
+#     },
+#     "Kichkina shahzoda": {
+#         "muallifi": "Antuan de Sent-Ekzyuperi",
+#         "janri": "Falsafiy ertak",
+#         "chop etilgan yili": 1943,
+#         "tarjimalar soni": 300
+#     }
+# }
+# book=input("Kitob nomini kiriting: ")
+# book=book.lower()
+# for i in books:
+#     if i.lower() == book:
+#         print(books[i])
